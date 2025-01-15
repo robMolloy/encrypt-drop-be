cf73870072ba95dc7b61fd7edf7e8868b7239603
robmolloy <robmolloy@hotmail.co.uk>
2025-01-15 15:55:12 +0000

    tidy cloud functiontesting

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,4 +1,7 @@
 import { initializeApp } from "firebase/app";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import { connectStorageEmulator, getStorage } from "firebase/storage";
 import { connectFunctionsEmulator, getFunctions } from "firebase/functions";
 
 const emulatorProjectId = "demo-project";
@@ -13,6 +16,12 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
 export const functions = getFunctions(app);
 
+connectAuthEmulator(auth, "http://127.0.0.1:9099");
+connectFirestoreEmulator(db, "127.0.0.1", 8080);
+connectStorageEmulator(storage, "127.0.0.1", 9199);
 connectFunctionsEmulator(functions, "127.0.0.1", 5001);
