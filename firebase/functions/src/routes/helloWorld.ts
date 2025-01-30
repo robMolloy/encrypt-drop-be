5a57ff0c54581257c4ed0f469a985c00ffa7c6ca
robmolloy <robmolloy@hotmail.co.uk>
2025-01-30 09:50:56 +0000

    remove unused cloud functions

--- firebase/functions/src/routes/helloWorld.ts
+++
@@ -1,5 +0,0 @@
-import { onCall } from "firebase-functions/v2/https";
-
-export const helloWorld = onCall(() => {
-  return { success: true, data: "Hello from Firebase!" };
-});
--- firebase/functions/src/routes/index.ts
+++ firebase/functions/src/routes/index.ts
@@ -1,5 +1,3 @@
 export * from "./createStripePaymentIntent";
-export * from "./helloWorld";
 export * from "./isPaymentIntentStatusSucceeded";
-export * from "./returnUid";
 export * from "./onCreatePaymentIntentDocUpdateBalanceIfValidAndReceipt";
